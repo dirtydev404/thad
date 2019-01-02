bf21aa8f3f69edba07e842f17f0fd7e72da95207
Thad <thadlounds@gmail.com>
2019-01-02 06:49:25 -0700

    added about page

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.send('about');
 });
 
 module.exports = router;
