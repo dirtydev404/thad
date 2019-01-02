6ed9551ae73aa869500472324b1b7d61acf6b181
Thad <thadlounds@gmail.com>
2019-01-02 15:00:23 -0700

    finished rought draft of about page

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(aboutRouter);
+app.get('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.send('about');
+  res.render(about);
 });
 
 module.exports = router;
